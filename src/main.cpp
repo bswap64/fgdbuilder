2e27562c85a655258a015fdb73850a85529deb1a
bswap64 <byteswap.c@gmail.com>
2026-06-14 00:18:25 +0400

    Add Discord RPC init and shutdown, bump version to 1.1.0, remove forced Fusion style

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <QApplication>
 #include "MainWindow.h"
+#include "DiscordRPC.h"
+
+static const int64_t DISCORD_CLIENT_ID = 1515443340865441892;
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    // fusion style is good (i guess... :/)
-    QApplication::setStyle("Fusion");
     app.setApplicationName("FGDBuilder");
-    app.setApplicationVersion("1.0");
+    app.setApplicationVersion("1.1.0");
     app.setOrganizationName("FGDBuilder");
+
+    DiscordRPC::instance().init(DISCORD_CLIENT_ID);
+
     MainWindow window;
     window.show();
-    return app.exec();
+
+    int ret = app.exec();
+    DiscordRPC::instance().shutdown();
+    return ret;
 }
